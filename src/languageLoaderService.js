9145bae3ddf9a441ad89335dc4f9ef5474d0d14e
Felix Steiner <felix@joinbox.com>
2014-08-15 20:30:00 +0200

    Initial commit

---
+++ src/languageLoaderService.js
@@ -0,0 +1,167 @@
+/**
+* Loads translations from server, caches them for 1h in localStorage, as the request ist pretty big (~400kb)
+* See http://angular-translate.github.io/docs/#/guide/13_custom-loaders
+*
+* localStorage: key is «locales», contains two fields: 
+* - fetchTimeStamp: timestamp the locales were fetched from server
+* - locales: locales gotten from server, formatted
+*/ 
+
+angular
+.module( "eb.languageLoader", [ "eb.config" ] )
+.factory( 'LanguageLoaderService', [ "$http", "$q", "BackendBaseUrl", function ( $http, $q, BackendBaseUrl ) {
+
+	// return loaderFn
+	return function ( options ) {
+
+		var deferred = $q.defer();
+
+		if( getLocalesFromStorage() ) {
+			deferred.resolve( getLocalesFromStorage() )
+		}
+		else {
+			getLocalesFromServer()
+				.then ( function( locales ) {
+					console.log( "Take locales from server: %o", locales );
+					deferred.resolve( locales )
+				}, function( err ) {
+					return $q.reject( err );
+				} )
+		}
+
+		return deferred.promise;
+
+	};
+
+
+
+
+
+	/**
+	* Tries to get locales from local storage; returns them or false
+	*/
+	function getLocalesFromStorage() {
+
+		var storedLocales = localStorage.getItem( "locales" );
+
+		// Not available
+		if( !storedLocales ) {
+			return false;
+		}
+
+		storedLocales = JSON.parse( storedLocales );
+
+		// Properties missing
+		if( !storedLocales.locales || !storedLocales.fetchTimeStamp ) {
+			console.warn( "Missng properties in localStorage.locales: " + JSON.stringify( storedLocales ) );
+			return false;
+		}
+
+		// Older than 1h
+		if( storedLocales.fetchTimeStamp < new Date().getTime() - 60*60*1000 ) {
+			console.log( "Locales older than 1h" );
+			return false;
+		}
+
+		// Wrong language
+		if( storedLocales.language !== $( "html" ).attr( 'lang' ) ) {
+			console.log( "Locales stored in wrong Language, request from server" );
+			return false;
+		}
+
+		console.log( "Take locales from localStorage: %o", storedLocales.locales );
+		return storedLocales.locales;
+
+	}
+
+
+
+
+	/**
+	* Gets locales from server, stores them in localStorage
+	* @return Promise
+	*/
+	function getLocalesFromServer() {
+
+		var headers = {
+			"filter" 	: "key=like('web.cornercard.%')"
+			, "range" 	: "0-6000"
+			, "select" 	: "key, resourceLocale.*, resourceLocale.language.*"
+		}
+
+		return $http( { method: "GET", url: BackendBaseUrl + "/resource?_noc=" + new Date().getTime(), headers: headers } )
+			.then( function( response ) {
+				
+				var formattedLocales = formatLocales( response.data );
+
+				// Put to localStorage
+				var toStore = {
+					fetchTimeStamp 		: new Date().getTime()
+					, locales 			: formattedLocales
+					, language 			: $( 'html' ).attr( 'lang' )
+				}
+				
+				console.log( 'Store %o in localStorage', toStore );
+				localStorage.setItem( 'locales', JSON.stringify( toStore ) );
+
+				// Resolve
+				return formattedLocales;
+
+			}, function( data ) {
+				
+				var message = "Couldn't load locales. Status: " + data.status + ". Message: " + JSON.stringify( data.data );
+				console.error( message );
+				$q.reject( { code: 'serverError', message: message } );
+
+			} );
+
+	}
+
+
+	/**
+	* Translates server data into flat object of translations
+	*/
+	function formatLocales( serverData ) {
+
+		var trans = {};
+
+		// Loop all translation keys
+		for( var i = 0; i < serverData.length; i++ ) {
+
+			var locale = serverData[ i ]
+				, lang = $( "html" ).attr( 'lang' );
+
+			// Get right language
+			if( !locale.resourceLocale || !locale.resourceLocale.length ) {
+				console.warn( "Proper translation missing for " + JSON.stringify( locale ) );
+				continue;
+			}
+
+			// Loop translations for a certain key
+			for( var j = 0; j < locale.resourceLocale.length; j++ ) {
+				var loc = locale.resourceLocale[ j ];
+				if( !loc.language || !loc.language.code ) {
+					console.error( "Missing language in localization " + JSON.stringify( locale ) );
+					continue;
+				}
+
+				// Correct language found
+				// break this inner loop
+				if( loc.language.code === lang ) {
+					trans[ locale.key ] = loc.value;
+					break;
+				}
+
+			}
+
+
+		}
+
+		console.log( "trans is %o", trans );
+
+		return trans;
+
+	}
+
+
+} ] );
